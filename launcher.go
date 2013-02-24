975c5f18f36b13d314ec5c72f24ce1e4268c2367
Joe Walnes <jwalnes@drwholdings.com>
2013-02-24 13:29:56 -0600

    Pulled a lot of code from main.go into launcher.go.

---
+++ launcher.go
@@ -0,0 +1,86 @@
+// Copyright 2013 Joe Walnes and the websocketd team.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import(
+	"errors"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+var ScriptNotFoundError = errors.New("script not found")
+
+type URLInfo struct {
+	ScriptPath string
+	PathInfo   string
+	FilePath   string
+}
+
+func parsePath(path string, config *Config) (*URLInfo, error) {
+	if !config.UsingScriptDir {
+		return &URLInfo{"/", path, ""}, nil
+	}
+	
+	parts := strings.Split(path[1:], "/")
+	urlInfo := &URLInfo{}
+
+	for i, part := range parts {
+		urlInfo.ScriptPath = strings.Join([]string{urlInfo.ScriptPath, part}, "/")
+		urlInfo.FilePath = filepath.Join(config.ScriptDir, urlInfo.ScriptPath)
+		isLastPart := i == len(parts) - 1
+		statInfo, err := os.Stat(urlInfo.FilePath)
+
+		// not a valid path
+		if err != nil {
+			return nil, ScriptNotFoundError
+		}
+
+		// at the end of url but is a dir
+		if isLastPart && statInfo.IsDir() {
+			return nil, ScriptNotFoundError
+		}
+
+		// we've hit a dir, carry on looking
+		if statInfo.IsDir() {
+			continue
+		} 
+
+		// no extra args
+		if isLastPart {
+			return urlInfo, nil
+		}
+
+		// build path info from extra parts of url
+		urlInfo.PathInfo = "/" + strings.Join(parts[i+1:], "/")
+		return urlInfo, nil
+	}
+	panic("parsePath")
+}
+
+func launchCmd(commandName string, commandArgs []string, env []string) (*exec.Cmd, io.WriteCloser, io.ReadCloser, error) {
+	cmd := exec.Command(commandName, commandArgs...)
+	cmd.Env = env
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return cmd, nil, nil, err
+	}
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return cmd, nil, nil, err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return cmd, nil, nil, err
+	}
+
+	return cmd, stdin, stdout, err
+}
--- main.go
+++ main.go
@@ -7,14 +7,9 @@ package main
 
 import (
 	"flag"
-	"io"
 	"log"
 	"net/http"
-	"os/exec"
 	"strings"
-	"path/filepath"
-	"os"
-	"errors"
 
 	"code.google.com/p/go.net/websocket"
 )
@@ -38,55 +33,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-var ScriptNotFoundError = errors.New("script not found")
-
-type URLInfo struct {
-	ScriptPath string
-	PathInfo   string
-	FilePath   string
-}
-
-func parsePath(path string, config *Config) (*URLInfo, error) {
-	if !config.UsingScriptDir {
-		return &URLInfo{"/", path, ""}, nil
-	}
-	
-	parts := strings.Split(path[1:], "/")
-	urlInfo := &URLInfo{}
-
-	for i, part := range parts {
-		urlInfo.ScriptPath = strings.Join([]string{urlInfo.ScriptPath, part}, "/")
-		urlInfo.FilePath = filepath.Join(config.ScriptDir, urlInfo.ScriptPath)
-		isLastPart := i == len(parts) - 1
-		statInfo, err := os.Stat(urlInfo.FilePath)
-
-		// not a valid path
-		if err != nil {
-			return nil, ScriptNotFoundError
-		}
-
-		// at the end of url but is a dir
-		if isLastPart && statInfo.IsDir() {
-			return nil, ScriptNotFoundError
-		}
-
-		// we've hit a dir, carry on looking
-		if statInfo.IsDir() {
-			continue
-		} 
-
-		// no extra args
-		if isLastPart {
-			return urlInfo, nil
-		}
-
-		// build path info from extra parts of url
-		urlInfo.PathInfo = "/" + strings.Join(parts[i+1:], "/")
-		return urlInfo, nil
-	}
-	panic("parsePath")
 }
 
 func acceptWebSocket(ws *websocket.Conn, config *Config) {
@@ -121,7 +67,7 @@ func acceptWebSocket(ws *websocket.Conn, config *Config) {
 		commandName = urlInfo.FilePath
 	}
 
-	_, stdin, stdout, err := execCmd(commandName, config.CommandArgs, env)
+	_, stdin, stdout, err := launchCmd(commandName, config.CommandArgs, env)
 	if err != nil {
 		if config.Verbose {
 			log.Print("process: Failed to start: ", err)
@@ -140,26 +86,4 @@ func acceptWebSocket(ws *websocket.Conn, config *Config) {
 	go writeProcess(stdin, inbound, done, config)
 
 	<-done
-}
-
-func execCmd(commandName string, commandArgs []string, env []string) (*exec.Cmd, io.WriteCloser, io.ReadCloser, error) {
-	cmd := exec.Command(commandName, commandArgs...)
-	cmd.Env = env
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return cmd, nil, nil, err
-	}
-
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		return cmd, nil, nil, err
-	}
-
-	err = cmd.Start()
-	if err != nil {
-		return cmd, nil, nil, err
-	}
-
-	return cmd, stdin, stdout, err
 }
