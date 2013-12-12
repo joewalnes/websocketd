3aa0593882ca5af0d672dfd9830a450a26e908db
Jeroen Janssens <jeroen@jeroenjanssens.com>
2013-12-12 15:55:45 -0500

    Ensure that tail and grep terminate as well

--- examples/bash/chat.sh
+++ examples/bash/chat.sh
@@ -9,5 +9,5 @@
 echo "Please enter your name:"; read USER
 echo "[$(date)] ${USER} joined the chat" >> chat.log
 echo "[$(date)] Welcome to the chat ${USER}!"
-tail -n 0 -f chat.log | grep --line-buffered -v "] ${USER}>" &
+tail -n 0 -f chat.log --pid=$$ | grep --line-buffered -v "] ${USER}>" &
 while read MSG; do echo "[$(date)] ${USER}> ${MSG}" >> chat.log; done
