8455dbb48a83738946d7422d022c01427abd2685
Jeroen Janssens <jeroen@jeroenjanssens.com>
2013-12-13 22:52:56 -0500

    Extend description with GNU tail requirement and disclaimer

--- examples/bash/chat.sh
+++ examples/bash/chat.sh
@@ -6,6 +6,12 @@
 # license that can be found in the LICENSE file.
 
 # Run a simple chat server: websocketd --devconsole --port 8080 ./chat.sh
+#
+# Please note that this example requires GNU tail, which is not the default
+# tail on OS X. Even though this script properly escapes the variables,
+# please keep in mind that it is in general a bad idea to read
+# untrusted data into variables and pass this onto the command line.
+
 echo "Please enter your name:"; read USER
 echo "[$(date)] ${USER} joined the chat" >> chat.log
 echo "[$(date)] Welcome to the chat ${USER}!"
