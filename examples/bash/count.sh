30b66ea8b6f90a02b836eaabbe6f2ddf825f8b55
Rüdiger Sonderfeld <ruediger@c-plusplus.net>
2015-02-16 12:26:09 +0100

    Improve bash examples.
    See also http://www.shellcheck.net/
    
    * examples/bash/count.sh,README.md: `seq` is nonstandard, inefficient.
      Use shell built-in loop feature instead.
    * examples/bash/dump-env.sh: Double quote to prevent globbing (SC2086)
    * examples/bash/send-receive.sh: Use _ for unused variable; Improved use
      of shell arithmetic ((cnt++)); Use $().

--- examples/bash/count.sh
+++ examples/bash/count.sh
@@ -6,7 +6,7 @@
 # license that can be found in the LICENSE file.
 
 # Simple example script that counts to 10 at ~2Hz, then stops.
-for COUNT in $(seq 1 10)
+for ((COUNT = 1; COUNT <= 10; COUNT++))
 do
   echo $COUNT
   sleep 0.5
