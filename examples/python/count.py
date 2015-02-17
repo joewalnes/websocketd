87a54d3908b675e9da26c8f175c6c9e410d75239
Markus Becker <markushx@gmail.com>
2015-02-17 20:12:25 +0100

    flush before sleep

--- examples/python/count.py
+++ examples/python/count.py
@@ -11,5 +11,5 @@ from time import sleep
 # Simple example script that counts to 10 at ~2Hz, then stops.
 for count in range(0, 10):
   print count + 1
-  sleep(0.5)
   stdout.flush() # Remember to flush
+  sleep(0.5)
