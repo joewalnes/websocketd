3a3970cde4dbfc91f9ec5ae826ee1ce15a495b96
Alex Sergeyev <abc@alexsergeyev.com>
2015-03-03 15:59:49 -0500

    Changed --version output for #123

--- version.go
+++ version.go
@@ -5,14 +5,17 @@
 
 package main
 
+import (
+	"fmt"
+	"runtime"
+)
+
 // This value can be set for releases at build time using:
-//   go {build|run} -ldflags "-X main.version 1.2.3.4".
+//   go {build|run} -ldflags "-X main.version 1.2.3 -X main.buildinfo timestamp-@githubuser-platform".
 // If unset, Version() shall return "DEVBUILD".
-var version string
+var version string = "DEVBUILD"
+var buildinfo string = "--"
 
 func Version() string {
-	if version == "" {
-		return "DEVBUILD"
-	}
-	return version
+	return fmt.Sprintf("%s (%s %s-%s) %s", version, runtime.Version(), runtime.GOOS, runtime.GOARCH, buildinfo)
 }
