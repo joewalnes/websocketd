726420b1b66dbdb73ae67e96bdfe1f4de02088da
yzprofile <yzprofiles@gmail.com>
2016-12-12 01:14:17 +0800

    fix commont of broadcast.sh

--- examples/bash/broadcast.sh
+++ examples/bash/broadcast.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Copyright 2013 Joe Walnes and the websocketd team.
-# All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
-
-# Simple example script that counts to 10 at ~2Hz, then stops.
 while true
 do
   echo `date`
