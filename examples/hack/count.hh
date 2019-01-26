4cd503b37105d921aafe74f37e885f070affad0a
Saif Eddin G <29315886+azjezz@users.noreply.github.com>
2019-01-26 19:55:37 +0100

    Create count.hh

---
+++ examples/hack/count.hh
@@ -0,0 +1,20 @@
+#!/usr/bin/hhvm
+<?hh // strict
+
+use function HH\Lib\Experimental\IO\request_output;
+use function usleep;
+
+// Simple example script that counts to 10 at ~2Hz, then stops.
+
+<<__EntryPoint>>
+async function count(): Awaitable<void> {
+  $output = request_output();
+  for ($count = 1; $count <= 10; $count++) {
+    await $output->writeAsync(
+      Str\format("%d\n",$count)
+    );
+ 
+    // usleep is builtin, it is not an async builtin - so it also must block the main request thread
+    usleep(500000);
+  }
+}
